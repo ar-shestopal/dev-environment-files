f438e2928d4d474368ff842a37c356445d8a5269
oshe <oleksandr.shestopal@outreach.io>
2024-04-28 12:12:06 +0300

    add goland vim

--- .config/nvim/lua/oshe/plugins/telescope.lua
+++ .config/nvim/lua/oshe/plugins/telescope.lua
@@ -21,9 +21,46 @@ return {
           },
         },
       },
+
+      file_ignore_patterns = {},
     })
 
     telescope.load_extension("fzf")
+
+    -- Custom file finder to include files with line numbers
+    local function find_files_with_line_numbers(opts)
+      local entries = {}
+
+      -- Run a custom command to list files
+      local command = "find . -type f"
+      local filelist = vim.fn.systemlist(command)
+
+      for _, file in ipairs(filelist) do
+        -- Check if the file name contains a line number
+        local file, line_number = file:match("^(.-):(%d+)$")
+        if file then
+          -- Add file with line number to the list of entries
+          table.insert(entries, {
+            value = file,
+            ordinal = file,
+            display = string.format("%s:%s", file, line_number),
+            filename = file,
+            lnum = tonumber(line_number),
+          })
+        else
+          -- Add regular file to the list of entries
+          table.insert(entries, {
+            value = file,
+            ordinal = file,
+            display = file,
+            filename = file,
+          })
+        end
+      end
+
+      -- Return the list of entries
+      return entries
+    end
 
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
@@ -38,6 +75,31 @@ return {
       builtin.grep_string({ search = vim.fn.input("Grep > ") })
     end)
 
+    -- Custom entry maker to parse file paths with line numbers
+    local function make_file_entry(entry) end
+
+    keymap.set("n", "<leader>ft", function()
+      builtin.find_files({
+        entry_maker = function(entry)
+          local file, line_number = entry:match("^(.-):(%d+)$")
+          if file then
+            return {
+              ordinal = file,
+              display = string.format("%s:%s", file, line_number),
+              filename = file,
+              lnum = tonumber(line_number),
+            }
+          else
+            return {
+              ordinal = entry,
+              display = entry,
+              filename = entry,
+              lnum = 10,
+            }
+          end
+        end,
+      })
+    end)
     -- local builtin = require('telescope.builtin')
     -- vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
     -- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
